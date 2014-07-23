c31eef9045ca0eb8850680f87549ac6267259f65
Shahar Kedar <shahar@gmail.com>
2014-07-23 14:24:57 +0300

    Script should be run from /ansible

--- ansible/bin/generate_role.sh
+++ ansible/bin/generate_role.sh
@@ -1,11 +1,11 @@
 role=$1
 
-mkdir -p ../roles/$role/tasks
-mkdir -p ../roles/$role/vars
-mkdir -p ../roles/$role/files
-mkdir -p ../roles/$role/templates
-mkdir -p ../roles/$role/handlers
-mkdir -p ../roles/$role/meta
+mkdir -p roles/$role/tasks
+mkdir -p roles/$role/vars
+mkdir -p roles/$role/files
+mkdir -p roles/$role/templates
+mkdir -p roles/$role/handlers
+mkdir -p roles/$role/meta
 
 
 
